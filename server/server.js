f776561bc982d4c5ca6ad11c4d4d1e0dccab7d98
valco1998 <akivaidan1998@gmail.com>
2024-12-17 15:29:56 +0200

    Updated server.js and App.jsx

--- server/server.js
+++ server/server.js
@@ -13,8 +13,6 @@ import { app, server } from "./socket/socket.js";
 
 dotenv.config();
 
-dotenv.config();
-
 const __dirname = path.resolve();
 // PORT should be assigned after calling dotenv.config() because we need to access the env variables. Didn't realize while recording the video. Sorry for the confusion.
 const PORT = process.env.PORT || 5000;
@@ -26,10 +24,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
 server.listen(PORT, () => {
